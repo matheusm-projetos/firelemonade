39f4ba6fd5bbd5fdb3890ab3cf8445667cb746fd
unknown <matheusm.projetos@gmail.com>
2025-06-25 00:28:30 -0300

    feat: carrega os Cogs, disponibilizando os comandos

--- database/db_manager.py
+++ database/db_manager.py
@@ -1,39 +1,61 @@
 import json
 import os
 
-DB_FILE = os.path.join('database', 'data', 'jogadores.json')
+JOGADOR_FILE = os.path.join('database', 'data', 'jogadores.json')
+USUARIOS_FILE = os.path.join('database', 'data', 'usuarios.json')
 
 #classe que gerencia o acesso e carregamento dos dados do jogo
 class DatabaseManager:
     def __init__(self):
-        self._jogadores = self.load_jogadores()
+        self._jogadores_data = self.load_json(JOGADOR_FILE, "jogadores")
+        self._usuarios_data = self.load_json(USUARIOS_FILE, "usuários")
     
     #carrega a lista de jogadores da base de dados    
-    def load_jogadores(self):
+    def load_json(self, file_path, data_name):
         try:
-            with open(DB_FILE, 'r', encoding = 'utf-8') as f:
-                print(" Carregando banco de dados de jogadores...")
+            with open(file_path, 'r', encoding = 'utf-8') as f:
+                print(f" Carregando banco de dados de {data_name}...")
                 data = json.load(f)
-                print(f"{len(data)} jogadores carregados com sucesso")
+                print(f"Dadis de {data_name} carregados com sucesso")
                 return data
         except FileNotFoundError:
-            print(f"Erro: o arquivo '{DB_FILE}' não foi encontrado")
+            print(f"Erro: o arquivo '{file_path}' não foi encontrado. Criando novo")
             return {}
         except json.JSONDecodeError:
-            print(f"Erro: O arquivo '{DB_FILE}' contém um erro de formatação JSON")
+            print(f"Erro: O arquivo '{file_path}' contém um erro de formatação JSON")
             return {}
+    
+    #salva os dados do usuário na base de dados    
+    def save_usuarios_data(self):
+        try:
+            with open(USUARIOS_FILE, "w", encoding = 'utf-8') as f:
+                json.dump(self._usuarios_data, f, indent = 4)
+        except Exception as e:
+            print(f"Erro ao salvar dados dos usuários: {e}")
         
+    #retorna a lista dos jogadores da base de dados
     def get_jogadores(self):
         return self._jogadores
     
-if __name__ == "__main__":
-    db = DatabaseManager()
+    def get_usuarios_data(self, user_id):
+        return self._usuarios_data.get(str(user_id))
     
-    todos_jogadores = db.get_jogadores()
-    
-    if todos_jogadores:
-        primeiro_jogador = todos_jogadores.get('1')
+    def create_user(self, user_id, username):
+        user_id_str = str(user_id)
+        if user_id_str in self._usuarios_data:
+            return False
         
-        if primeiro_jogador:
-            print("\nTeste de carregamento:")
-            print(f"Nome do primeiro jogador carregado: {primeiro_jogador.get('nome')}")
+        self._usuarios_data[user_id_str] = {
+            "username": username,
+            "elenco": {},
+            "times": {},
+            "moedas": {
+                "pontos_espirito": 0,
+                "relampagos_dourados": 0,
+                "medalhas_raimon": 0
+            }
+        }
+        
+        self.save_usuarios_data()
+        return True
+            
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import discord
 from discord.ext import commands
+import os
+import asyncio
+
 import config
+from database.db_manager import DatabaseManager
 
 intents = discord.Intents.default()
 
@@ -8,7 +12,24 @@ intents.members = True
 
 intents.message_content = True
 
-bot = commands.Bot(command_prefix = config.BOT_PREFIX, intents = intents)
+class FirelemonadeBot(commands.Bot):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.db_manager = DatabaseManager()
+
+    async def setup_hook(self):
+        print("carregando cogs...")
+        for filename in os.listdir('./cogs'):
+            if filename.endswith('.py'):
+                cog_name = f'cogs.{filename[:-3]}'
+                try:
+                    await self.load_extension(cog_name)
+                    print(f'->cog "{cog_name}" carregando')
+                except Exception as e:
+                    print(f'Erro ao carregar o cog "{cog_name}": {e}')
+        print("cogs carregados")
+
+bot = FirelemonadeBot(command_prefix=config.BOT_PREFIX, intents=intents)
 
 #evento que é iniciado quando o bot está pronto pra ficar online
 @bot.event
@@ -22,4 +43,7 @@ async def on_ready():
     
 #ponto de início - inicializa o bot
 if __name__ == "__main__":
-    bot.run(config.DISCORD_TOKEN)
+    async def main():
+        await bot.start(config.DISCORD_TOKEN)
+
+    asyncio.run(main())
